72b291c12ea52e8b85ed5ebbb17d022ed3182505
Tigran <harutyunyan.tigran1975@gmail.com>
2024-10-21 12:58:15 +0400

    created CreateWorkspaceModal component

---
+++ features/workspaces/components/CreateWorkspaceModal.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import CreateWorkspaceForm from "../components/CreateWorkspaceForm.vue";
+import ResponsiveModal from "@/components/ResponsiveModal.vue";
+import { useCreateWorkspaceModal } from "../store/useCreateWorkspaceModal";
+
+const { onClose } = useCreateWorkspaceModal();
+const { isOpen } = storeToRefs(useCreateWorkspaceModal());
+</script>
+
+<template>
+  <ResponsiveModal :open="isOpen" @onOpenChange="onClose">
+    <CreateWorkspaceForm @onClose="onClose" />
+  </ResponsiveModal>
+</template>
