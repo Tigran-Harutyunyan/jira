6139bd465e5e094ecae0aac4a685bfdf58ce3562
Tigran <harutyunyan.tigran1975@gmail.com>
2024-10-21 12:57:21 +0400

    created responsive modal component

---
+++ components/ResponsiveModal.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { Dialog, DialogContent } from "@/components/ui/dialog";
+import { Drawer, DrawerContent } from "@/components/ui/drawer";
+import { useWindowSize } from "@vueuse/core";
+
+const emit = defineEmits<{
+  (e: "onOpenChange"): void;
+}>();
+
+interface ResponsiveModalProps {
+  open: boolean;
+}
+
+defineProps<ResponsiveModalProps>();
+
+const { width } = useWindowSize();
+const isDesktop = computed(() => width.value > 1024);
+
+let resolvePromise: (value: PromiseLike<boolean> | boolean) => void;
+
+const openModal = () => {
+  return new Promise((resolve) => {
+    resolvePromise = resolve;
+  });
+};
+
+defineExpose({ openModal });
+</script>
+
+<template>
+  <Dialog v-if="isDesktop" :open="open" @update:open="emit('onOpenChange')">
+    <DialogContent
+      class="w-full sm:max-w-lg p-0 border-none overflow-y-auto hide-scrollbar max-h-[85vh]"
+    >
+      <slot />
+    </DialogContent>
+  </Dialog>
+
+  <Drawer v-else :open="open" @update:open="emit('onOpenChange')">
+    <DrawerContent>
+      <div class="overflow-y-auto hide-scrollbar max-h-[85vh]">
+        <slot />
+      </div>
+    </DrawerContent>
+  </Drawer>
+</template>
