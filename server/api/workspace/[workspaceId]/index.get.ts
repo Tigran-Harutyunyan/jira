3af5b2db8bbaf9a68a25e083c92eb7508681f5a8
Tigran <harutyunyan.tigran1975@gmail.com>
2024-10-22 09:11:49 +0400

    created GET workspace endpoint

---
+++ server/api/workspace/[workspaceId]/index.get.ts
@@ -0,0 +1,48 @@
+import { createSessionClient } from "@/lib/appwrite";
+import { Query } from "node-appwrite";
+import { type Workspace } from '@/features/workspaces/types';
+
+const runtimeConfig = useRuntimeConfig();
+const DATABASE_ID = runtimeConfig.APPWRITE_DATABASE_ID;
+const WORKSPACES_ID = runtimeConfig.public.appwrite.APPWRITE_WORKSPACES_ID;
+const MEMBERS_ID = runtimeConfig.public.appwrite.APPWRITE_MEMBERS_ID;
+
+export default defineEventHandler(async (event) => {
+
+    try {
+        const { account, databases } = await createSessionClient();
+
+        const user = await account.get();
+
+        const params = event.context.params;
+
+        const workspaceId = params && 'workspaceId' in params ? params.workspaceId : '';
+
+        const members = await databases.listDocuments(
+            DATABASE_ID,
+            MEMBERS_ID,
+            [Query.equal("userId", user.$id)]
+        );
+
+        if (!members.documents[0]) {
+            return createError({
+                statusCode: 401,
+                statusMessage: 'Unauthorized'
+            })
+        }
+
+        const workspace = await databases.getDocument<Workspace>(
+            DATABASE_ID,
+            WORKSPACES_ID,
+            workspaceId,
+        );
+
+        return { workspace };
+
+    } catch (error) {
+        return createError({
+            statusCode: 500,
+            statusMessage: error?.message
+        })
+    }
+}); 
