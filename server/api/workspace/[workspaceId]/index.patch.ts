8c7345e94a817248b7d18c6741fbafaf1e3c0d1d
Tigran <harutyunyan.tigran1975@gmail.com>
2024-10-22 09:12:30 +0400

    created UPDATE workspace endpoint

---
+++ server/api/workspace/[workspaceId]/index.patch.ts
@@ -0,0 +1,77 @@
+import { createSessionClient } from "@/lib/appwrite";
+import { Query } from "node-appwrite";
+import { ID } from "node-appwrite";
+import { type Workspace } from '@/features/workspaces/types';
+
+const runtimeConfig = useRuntimeConfig();
+const DATABASE_ID = runtimeConfig.APPWRITE_DATABASE_ID;
+const WORKSPACES_ID = runtimeConfig.public.appwrite.APPWRITE_WORKSPACES_ID;
+const MEMBERS_ID = runtimeConfig.public.appwrite.APPWRITE_MEMBERS_ID;
+const IMAGES_BUCKET_ID = runtimeConfig.public.appwrite.APPWRITE_IMAGES_BUCKET_ID;
+
+export default defineEventHandler(async (event) => {
+
+    try {
+        const formData = await readFormData(event);
+        const name = formData.get('name');
+        const image = formData.get('image') ?? '';
+
+        const { account, databases, storage } = await createSessionClient();
+
+        const user = await account.get();
+
+        const params = event.context.params;
+
+        const workspaceId = params && 'workspaceId' in params ? params.workspaceId : '';
+
+        const members = await databases.listDocuments(
+            DATABASE_ID,
+            MEMBERS_ID,
+            [Query.equal("userId", user.$id)]
+        );
+
+        if (!members.documents[0]) {
+            return createError({
+                statusCode: 401,
+                statusMessage: 'Unauthorized'
+            })
+        }
+
+        let uploadedImageUrl: string | undefined;
+
+        if (image instanceof File) {
+            const file = await storage.createFile(
+                IMAGES_BUCKET_ID,
+                ID.unique(),
+                image,
+            );
+
+            const arrayBuffer = await storage.getFilePreview(
+                IMAGES_BUCKET_ID,
+                file.$id,
+            );
+
+            uploadedImageUrl = `data:image/png;base64,${Buffer.from(arrayBuffer).toString("base64")}`;
+        } else {
+            uploadedImageUrl = image;
+        }
+
+        const workspace = await databases.updateDocument<Workspace>(
+            DATABASE_ID,
+            WORKSPACES_ID,
+            workspaceId,
+            {
+                name,
+                imageUrl: uploadedImageUrl
+            }
+        );
+
+        return { workspace };
+
+    } catch (error) {
+        return createError({
+            statusCode: 500,
+            statusMessage: error?.message
+        })
+    }
+}); 
