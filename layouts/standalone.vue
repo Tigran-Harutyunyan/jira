fa88a6853f4e545a4695b3399e3ef23e09d1ec7e
Tigran <harutyunyan.tigran1975@gmail.com>
2024-10-22 09:10:22 +0400

    created standalone layout

---
+++ layouts/standalone.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import UserButton from "@/features/auth/components/UserButton.vue";
+</script>
+
+<template>
+  <main class="bg-neutral-100 min-h-screen">
+    <div class="mx-auto max-w-screen-2xl p-4">
+      <nav class="flex justify-between items-center h-[73px]">
+        <NuxtLink to="/">
+          <NuxtImg src="/logo.svg" alt="Logo" :height="56" :width="152" />
+        </NuxtLink>
+        <UserButton />
+      </nav>
+      <div class="flex flex-col items-center justify-center py-4">
+        <slot />
+      </div>
+    </div>
+  </main>
+</template>
