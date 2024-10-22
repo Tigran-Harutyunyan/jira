b1f7919152d4df4e0b9d83e290286b358008e6b3
Tigran <harutyunyan.tigran1975@gmail.com>
2024-10-22 09:08:23 +0400

    created page loader component

---
+++ components/PageError.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { AlertTriangle } from "lucide-vue-next";
+
+interface PageErrorProps {
+  message: string;
+}
+
+withDefaults(defineProps<PageErrorProps>(), {
+  message: "Something went wrong",
+});
+</script>
+
+<template>
+  <div class="flex flex-col items-center justify-center h-full">
+    <AlertTriangle class="size-6 text-muted-foreground mb-2" />
+    <p class="text-sm font-medium text-muted-foreground">
+      {{ message }}
+    </p>
+  </div>
+</template>
