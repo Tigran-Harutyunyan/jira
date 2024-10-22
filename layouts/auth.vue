3dd3bd243c6b74403653fa4aaddcf86e8f2de99f
Tigran <harutyunyan.tigran1975@gmail.com>
2024-10-22 09:10:34 +0400

    created auth layout

---
+++ layouts/auth.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { Button } from "@/components/ui/button";
+
+const isSignIn = computed(() => useRoute().path === "/sign-in");
+</script>
+
+<template>
+  <main class="bg-neutral-100 min-h-screen">
+    <div class="mx-auto max-w-screen-2xl p-4">
+      <nav class="flex justify-between items-center">
+        <NuxtImg src="/logo.svg" alt="logo" width="152" height="56" />
+        <Button as-child variant="secondary">
+          <NuxtLink :to="isSignIn ? '/sign-up' : '/sign-in'">
+            {{ isSignIn ? "Sign Up" : "Login" }}
+          </NuxtLink>
+        </Button>
+      </nav>
+      <div class="flex flex-col items-center justify-center pt-4 md:pt-14">
+        <slot />
+      </div>
+    </div>
+  </main>
+</template>
