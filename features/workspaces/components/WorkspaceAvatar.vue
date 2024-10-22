f55641204d2b62be62e9708e9abb89728e541d13
Tigran <harutyunyan.tigran1975@gmail.com>
2024-10-22 11:39:06 +0400

    created WorkspaceAvatar component

---
+++ features/workspaces/components/WorkspaceAvatar.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { cn } from "@/lib/utils";
+
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+
+interface WorkspaceAvatarProps {
+  image?: string;
+  name: string;
+  className?: string;
+}
+
+defineProps<WorkspaceAvatarProps>();
+</script>
+
+<template>
+  <div
+    v-if="image"
+    :class="cn('size-8 relative rounded-md overflow-hidden', className)"
+  >
+    <NuxtImg :src="image" :alt="name" fill class="object-cover" />
+  </div>
+  <Avatar v-else :class="cn('size-8 rounded-md', className)">
+    <AvatarFallback
+      class="text-white bg-blue-600 font-semibold text-lg uppercase rounded-md"
+    >
+      {{ name[0] }}
+    </AvatarFallback>
+  </Avatar>
+</template>
