125e05e210dc50b89df309578cfcf28d2e59d055
Tigran <harutyunyan.tigran1975@gmail.com>
2024-10-22 09:10:11 +0400

    created dashboard layout

---
+++ layouts/dashboard.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import CreateWorkspaceModal from "@/features/workspaces/components/CreateWorkspaceModal.vue";
+</script>
+
+<template>
+  <ClientOnly>
+    <CreateWorkspaceModal />
+    <div class="min-h-screen">
+      <div class="flex w-full h-full">
+        <div
+          class="fixed left-0 top-0 hidden lg:block lg:w-[264px] h-full overflow-y-auto"
+        >
+          <Sidebar />
+        </div>
+        <div class="lg:pl-[264px] w-full">
+          <div class="mx-auto max-w-screen-2xl h-full">
+            <NavBar />
+            <main class="h-full py-8 px-6 flex flex-col">
+              <slot />
+            </main>
+          </div>
+        </div>
+      </div></div
+  ></ClientOnly>
+</template>
