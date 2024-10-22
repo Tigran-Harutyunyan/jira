0086ade4c998519cba25ac2350f13f39c04410a3
Tigran <harutyunyan.tigran1975@gmail.com>
2024-10-22 09:14:08 +0400

    created PageLoader component

---
+++ components/PageLoader.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { Loader } from "lucide-vue-next";
+</script>
+
+<template>
+  <div class="flex items-center justify-center h-screen">
+    <Loader class="size-6 animate-spin text-muted-foreground" />
+  </div>
+</template>
