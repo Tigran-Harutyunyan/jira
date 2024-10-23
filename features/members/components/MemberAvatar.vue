19f43d681be8930860b18297413e0d3c0c69a88b
Tigran <harutyunyan.tigran1975@gmail.com>
2024-10-23 13:00:05 +0400

    created MemberAvatar component

---
+++ features/members/components/MemberAvatar.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { cn } from "@/lib/utils";
+
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+
+interface MemberAvatarProps {
+  name: string;
+  className?: string;
+  fallbackClassName?: string;
+}
+
+defineProps<MemberAvatarProps>();
+</script>
+
+<template>
+  <Avatar
+    :class="
+      cn('size-5 transition border border-neutral-300 rounded-full', className)
+    "
+  >
+    <AvatarFallback
+      :class="
+        cn(
+          'bg-neutral-200 font-medium text-neutral-500 flex items-center justify-center',
+          fallbackClassName
+        )
+      "
+    >
+      {{ name.charAt(0).toUpperCase() }}
+    </AvatarFallback>
+  </Avatar>
+</template>
