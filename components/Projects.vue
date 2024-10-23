0fb4c56c98072a3b4a659cf932db5101775f74cc
Tigran <harutyunyan.tigran1975@gmail.com>
2024-10-23 13:06:34 +0400

    created Projects sidebar component

---
+++ components/Projects.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+import { RiAddCircleFill } from "vue3-icons/ri";
+import { useCreateProjectModal } from "@/features/projects/store/useCreateProjectModal";
+import ProjectAvatar from "@/features/projects/components/ProjectAvatar.vue";
+import { useWorkspaceId } from "@/features/workspaces/composables/useWorkspaceId";
+import { cn } from "@/lib/utils";
+
+const { onOpen } = useCreateProjectModal();
+const workspaceId = useWorkspaceId();
+
+const { data } = useQuery({
+  queryKey: ["projects", useWorkspaceId],
+  queryFn: async () => await $fetch(`/api/workspace/${workspaceId}/projects`),
+});
+
+const projects = computed(() => {
+  if (!data.value?.projects?.documents) return [];
+  const pathname = useRoute().params?.projectId;
+  return data.value?.projects?.documents.map((project) => {
+    const href = `/workspaces/${workspaceId}/projects/${project.$id}`;
+    const isActive = pathname === project.$id;
+    return {
+      ...project,
+      href,
+      isActive,
+    };
+  });
+});
+</script>
+
+<template>
+  <div class="flex flex-col gap-y-2">
+    <div class="flex items-center justify-between">
+      <p class="text-xs uppercase text-neutral-500">Projects</p>
+      <RiAddCircleFill
+        @click="onOpen()"
+        class="size-5 text-neutral-500 cursor-pointer hover:opacity-75 transition"
+      />
+    </div>
+
+    <NuxtLink v-for="project in projects" :to="project.href" :key="project.$id">
+      <div
+        :class="
+          cn(
+            'flex items-center gap-2.5 p-2.5 rounded-md hover:opacity-75 transition cursor-pointer text-neutral-500',
+            project.isActive &&
+              'bg-white shadow-sm hover:opacity-100 text-primary'
+          )
+        "
+      >
+        <ProjectAvatar :image="project.imageUrl" :name="project.name" />
+        <span class="truncate">{{ project.name }}</span>
+      </div>
+    </NuxtLink>
+  </div>
+</template>
