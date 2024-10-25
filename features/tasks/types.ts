a60d02f4baffa7f7e6d7c2bcdb474a823956fe09
Tigran <harutyunyan.tigran1975@gmail.com>
2024-10-25 15:48:45 +0400

    created types and schemas for tasks

---
+++ features/tasks/types.ts
@@ -0,0 +1,27 @@
+import { type Models } from "node-appwrite";
+
+export enum TaskStatus {
+  BACKLOG = "BACKLOG",
+  TODO = "TODO",
+  IN_PROGRESS = "IN_PROGRESS",
+  IN_REVIEW = "IN_REVIEW",
+  DONE = "DONE"
+};
+
+export type Task = Models.Document & {
+  name: string;
+  status: TaskStatus;
+  workspaceId: string;
+  assigneeId: string;
+  projectId: string;
+  position: number;
+  dueDate: string;
+  description?: string;
+};
+
+export type TaskFilters = {
+  status: TaskStatus | null;
+  assigneeId: string;
+  projectId?: string;
+  dueDate: string;
+};
