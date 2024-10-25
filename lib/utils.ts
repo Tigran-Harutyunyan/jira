efaf5f8477f6355ec3cb28d3cba260d3177e0db6
Tigran <harutyunyan.tigran1975@gmail.com>
2024-10-25 15:51:50 +0400

    created utils

---
+++ lib/utils.ts
@@ -0,0 +1,42 @@
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export const formatDate = (date: string) => {
+  const newDate = new Date(date)
+  return newDate.toLocaleDateString();
+}
+
+export const getClientCookie = (cname: string) => {
+  if (!document) return '';
+  let name = cname + "=";
+  let ca = document.cookie.split(';');
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+};
+
+export function generateInviteCode(length: number) {
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+};
+
+export function snakeCaseToTitleCase(str: string) {
+  return str.toLowerCase()
+    .replace(/_/g, " ")
+    .replace(/\b\w/g, (char) => char.toUpperCase())
+};
