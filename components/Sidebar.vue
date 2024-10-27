ee63fdd01c1aa7334982fb91907e4c76186273c0
Tigran <harutyunyan.tigran1975@gmail.com>
2024-10-27 12:07:25 +0400

    created Sidebar component

---
+++ components/Sidebar.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <aside class="h-full bg-neutral-100 p-4 w-full">
+    <NuxtLink to="/">
+      <NuxtImg src="/logo.svg" alt="logo" :width="164" :height="48" />
+    </NuxtLink>
+    <DottedSeparator class="my-4" />
+    <WorkspaceSwitcher />
+    <DottedSeparator class="my-4" />
+    <Navigation />
+    <DottedSeparator class="my-4" />
+    <Projects />
+  </aside>
+</template>
