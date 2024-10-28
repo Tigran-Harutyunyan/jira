c946aa346cc6d36040c59d62bd5d93033bac81ac
Tigran <harutyunyan.tigran1975@gmail.com>
2024-10-28 11:22:58 +0400

    taskDate component  reactivity fix

--- features/tasks/components/columns/TaskDateCell.vue
+++ features/tasks/components/columns/TaskDateCell.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { differenceInDays, format } from "date-fns";
-
 import { cn } from "@/lib/utils";
 
 interface TaskDateProps {
@@ -10,18 +9,22 @@ interface TaskDateProps {
 
 const props = defineProps<TaskDateProps>();
 
-const today = new Date();
-const endDate = new Date(props.value);
-const diffInDays = differenceInDays(endDate, today);
+const textColor = computed(() => {
+  const today = new Date();
+  const endDate = new Date(props.value);
+  const diffInDays = differenceInDays(endDate, today);
 
-let textColor = "text-muted-foreground";
-if (diffInDays <= 3) {
-  textColor = "text-red-500";
-} else if (diffInDays <= 7) {
-  textColor = "text-orange-500";
-} else if (diffInDays <= 14) {
-  textColor = "text-yellow-500";
-}
+  let textColor = "text-muted-foreground";
+
+  if (diffInDays <= 3) {
+    textColor = "text-red-500";
+  } else if (diffInDays <= 7) {
+    textColor = "text-orange-500";
+  } else if (diffInDays <= 14) {
+    textColor = "text-yellow-500";
+  }
+  return textColor;
+});
 </script>
 
 <template>
