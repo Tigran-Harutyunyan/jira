cd0e550f8cdd32bef47f757388b45497355f89bb
Tigran <harutyunyan.tigran1975@gmail.com>
2024-10-29 00:26:44 +0400

    created analytics components

---
+++ components/Analytics.vue
@@ -0,0 +1,75 @@
+<script setup lang="ts">
+import { ScrollArea, ScrollBar } from "./ui/scroll-area";
+import AnalyticsCard from "./AnalyticsCard.vue";
+import DottedSeparator from "./DottedSeparator.vue";
+
+interface ProjectAnalyticsResponseType {
+  data: {
+    taskCount: number;
+    taskDifference: number;
+    assignedTaskCount: number;
+    assignedTaskDifference: number;
+    completedTaskCount: number;
+    completedTaskDifference: number;
+    incompleteTaskCount: number;
+    incompleteTaskDifference: number;
+    overdueTaskCount: number;
+    overdueTaskDifference: number;
+  };
+}
+
+defineProps<ProjectAnalyticsResponseType>();
+</script>
+
+<template>
+  <ScrollArea class="border rounded-lg w-full whitespace-nowrap shrink-0">
+    <div class="w-full flex flex-row">
+      <div class="flex items-center flex-1">
+        <AnalyticsCard
+          title="Total tasks"
+          :value="data.taskCount"
+          :variant="data.taskDifference > 0 ? 'up' : 'down'"
+          :increaseValue="data.taskDifference"
+        />
+        <DottedSeparator direction="vertical" />
+      </div>
+      <div class="flex items-center flex-1">
+        <AnalyticsCard
+          title="Assigned Tasks"
+          :value="data.assignedTaskCount"
+          :variant="data.assignedTaskDifference > 0 ? 'up' : 'down'"
+          :increaseValue="data.assignedTaskDifference"
+        />
+        <DottedSeparator direction="vertical" />
+      </div>
+      <div class="flex items-center flex-1">
+        <AnalyticsCard
+          title="Completed Tasks"
+          :value="data.completedTaskCount"
+          :variant="data.completedTaskDifference > 0 ? 'up' : 'down'"
+          :increaseValue="data.completedTaskDifference"
+        />
+        <DottedSeparator direction="vertical" />
+      </div>
+      <div class="flex items-center flex-1">
+        <AnalyticsCard
+          title="Overdue Tasks"
+          :value="data.overdueTaskCount"
+          :variant="data.overdueTaskDifference > 0 ? 'up' : 'down'"
+          :increaseValue="data.overdueTaskDifference"
+        />
+        <DottedSeparator direction="vertical" />
+      </div>
+      <div class="flex items-center flex-1">
+        <AnalyticsCard
+          title="Incomplete Tasks"
+          :value="data.incompleteTaskCount"
+          :variant="data.incompleteTaskDifference > 0 ? 'up' : 'down'"
+          :increaseValue="data.incompleteTaskDifference"
+        />
+        <DottedSeparator direction="vertical" />
+      </div>
+    </div>
+    <ScrollBar orientation="horizontal" />
+  </ScrollArea>
+</template>
---
+++ components/AnalyticsCard.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { FaCaretDown, FaCaretUp } from "vue3-icons/fa";
+
+import { cn } from "@/lib/utils";
+import {
+  Card,
+  CardHeader,
+  CardDescription,
+  CardTitle,
+} from "@/components/ui/card";
+
+interface AnalyticsCardProps {
+  title: string;
+  value: number;
+  variant: "up" | "down";
+  increaseValue: number;
+}
+
+const props = defineProps<AnalyticsCardProps>();
+
+const iconColor = props.variant === "up" ? "text-emerald-500" : "text-red-500";
+const increaseValueColor =
+  props.variant === "up" ? "text-emerald-500" : "text-red-500";
+const Icon = props.variant === "up" ? FaCaretUp : FaCaretDown;
+</script>
+
+<template>
+  <Card class="shadow-none border-none w-full">
+    <CardHeader>
+      <div class="flex items-center gap-x-2.5">
+        <CardDescription
+          class="flex items-center gap-x-2 font-medium overflow-hidden"
+        >
+          <span class="truncate text-base">{{ title }}</span>
+        </CardDescription>
+        <div class="flex items-center gap-x-1">
+          <component :is="Icon" :class="cn(iconColor, 'size-4')" />
+          <span
+            :class="cn(increaseValueColor, 'truncate text-base font-medium')"
+          >
+            {{ increaseValue }}
+          </span>
+        </div>
+      </div>
+      <CardTitle class="3xl font-semibold">{{ value }}</CardTitle>
+    </CardHeader>
+  </Card>
+</template>
