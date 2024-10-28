2d4ed4bc767a45cf3affe1e6967c1b39a6d3618d
Tigran <harutyunyan.tigran1975@gmail.com>
2024-10-29 00:24:26 +0400

    created workspace members page

---
+++ pages/workspaces/[workspaceId]/members/index.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+definePageMeta({ middleware: "auth", auth: { guestRedirectUrl: "/sign-in" } });
+
+import MembersList from "@/features/workspaces/components/MembersList.vue";
+</script>
+<template>
+  <NuxtLayout name="standalone">
+    <div class="w-full lg:max-w-xl">
+      <MembersList />
+    </div>
+  </NuxtLayout>
+</template>
