044af3fe39e53ed8b5c938ef2b620e465728de5c
Tigran <harutyunyan.tigran1975@gmail.com>
2024-10-29 12:41:28 +0400

    commited page files,logo, and config

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+definePageMeta({
+  middleware: ["auth"],
+  auth: { guestRedirectUrl: "/sign-in" },
+});
+</script>
+<template>
+  <NuxtLayout name="dashboard"> </NuxtLayout>
+</template>
---
+++ pages/workspaces/index.vue
@@ -0,0 +1,7 @@
+<script lang="ts" setup>
+definePageMeta({ middleware: "auth", auth: { guestRedirectUrl: "/sign-in" } });
+</script>
+
+<template>
+  <NuxtLayout name="dashboard"> </NuxtLayout>
+</template>
