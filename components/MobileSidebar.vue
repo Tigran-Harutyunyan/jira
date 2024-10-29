0f0d69f903f3155daa8debd65ebbedc709b6db63
Tigran <harutyunyan.tigran1975@gmail.com>
2024-10-29 12:42:23 +0400

    created mobile sidebar

---
+++ components/MobileSidebar.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { MenuIcon } from "lucide-vue-next";
+import Sidebar from "./Sidebar.vue";
+import { Button } from "./ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+
+const isOpen = ref(false);
+
+watch(
+  () => useRoute().path,
+  () => {
+    isOpen.value = false;
+  }
+);
+</script>
+
+<template>
+  <Sheet :modal="false" :open="isOpen" @update:open="isOpen = !isOpen">
+    <SheetTrigger as-child>
+      <Button variant="secondary" class="lg:hidden">
+        <MenuIcon class="size-4 text-neutral-500" />
+      </Button>
+    </SheetTrigger>
+    <SheetContent side="left" class="p-0">
+      <Sidebar />
+    </SheetContent>
+  </Sheet>
+</template>
